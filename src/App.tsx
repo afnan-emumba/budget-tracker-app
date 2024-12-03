ae13607c03ebdbfe2c980fd1f4b379e7c0e76108
afnan-emumba <afnan.sohail@emumba.com>
2024-12-03 14:48:24 +0500

    added dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ConfigProvider } from "antd";
+
+import Signup from "./pages/auth/signup/Signup";
+import Login from "./pages/auth/login/Login";
+import Home from "./pages/home/Home";
+import AuthLayout from "./layouts/authLayout/AuthLayout";
+import MainLayout from "./layouts/mainLayout/MainLayout";
+import { antTheme } from "./theme/theme";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <ConfigProvider theme={antTheme}>
+      <div className='app-container'>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AuthLayout />}>
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/login' element={<Login />} />
+            </Route>
+
+            <Route element={<MainLayout />}>
+              <Route path='/' element={<Home />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ConfigProvider>
   );
 }
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+export const antTheme = {
+  token: {
+    colorPrimary: "#7539ff",
+    colorInfo: "#7539ff",
+    colorSuccess: "#08b461",
+    colorWarning: "#f7bb43",
+    colorError: "#ef4435",
+    borderRadius: 4,
+  },
+};
