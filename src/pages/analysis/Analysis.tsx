09d691aa63205b2601d7c9fd829981e5359b9925
afnan-emumba <afnan.sohail@emumba.com>
2024-12-10 18:49:02 +0500

    started analysis page

--- src/pages/analysis/Analysis.tsx
+++ src/pages/analysis/Analysis.tsx
@@ -1,7 +1,9 @@
 import { Helmet } from "react-helmet";
-import { Divider } from "antd";
+import { Divider, Table, Select } from "antd";
 
 import "./Analysis.css";
+
+const { Option } = Select;
 
 const Analysis = () => {
   return (
@@ -18,7 +20,23 @@ const Analysis = () => {
         <Divider />
 
         <div className='analysis-graph'>
-          <h3>Graph</h3>
+          <Table
+            showHeader={false}
+            title={() => (
+              <div className='table-controls'>
+                <h3>Expenses</h3>
+
+                <div className='table-select'>
+                  <label>Sort by</label>
+                  <Select defaultValue='' style={{ width: 160 }}>
+                    <Option value='1'>Last Month</Option>
+                    <Option value='6'>Last 6 Months</Option>
+                    <Option value='12'>Last 12 Months</Option>
+                  </Select>
+                </div>
+              </div>
+            )}
+          />
         </div>
       </div>
     </>
